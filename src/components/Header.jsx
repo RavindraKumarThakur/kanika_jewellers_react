7a538920546697520a6591ee42455e185c792313
Ravindra Kumar <kumarravindra54787@gmail.com>
2026-03-26 19:00:47 +0530

    Created header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,43 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 function Header() {
   const { count } = useCart();
 
+  const navigation_tabs = [
+    {
+      name: "Home",
+      link: '/'
+    },
+    {
+      name: 'collections',
+      link: '/shop'
+    },
+    {
+      name: 'about us',
+      link: '/about'
+    },
+    {
+      name: 'contact',
+      link: '/contact'
+    }
+  ]
+
+  const profile_tabs = [
+    {
+      name: "Sign in",
+      icon: "",
+      link: '/account'
+    },
+    {
+      name: "wishlist",
+      icon: "",
+      link: '/account'
+    },
+    {
+      name: "cart",
+      icon: "",
+      link: ''
+    }
+  ]
+
   return (
     <header className={styles.header}>
 
@@ -19,7 +56,11 @@ function Header() {
           <p>Free shipping from above <span>AUD 400!</span></p>
           <div className={styles["input-container"]}>
             <input type="text" placeholder='Find our product' />
-            <span className={styles.search}><img src={Search} alt='#' /></span>
+            <span className={styles['']}>
+              <svg width="15" height="15" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M42 42L33.3 33.3M38 22C38 30.8366 30.8366 38 22 38C13.1634 38 6 30.8366 6 22C6 13.1634 13.1634 6 22 6C30.8366 6 38 13.1634 38 22Z" stroke="#ffffff" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </span>
           </div>
         </div>
 
@@ -43,7 +84,14 @@ function Header() {
               <Link to={'/'}>
                 <img src={Kanika_logo} alt='#' />
               </Link>
+            </div>
 
+            <div className={styles['navigation-tabs']}>
+              {navigation_tabs.map((items) => (
+                <Link to={items.link}>
+                  {items.name.toUpperCase()}
+                </Link>
+              ))}
             </div>
           </div>
 
@@ -60,12 +108,72 @@ function Header() {
                   </svg>
                 </span>
               </div>
+              <div className={styles['profile']}>
+                <span>
+                  <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                </span>
+                <span>
+                  <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                </span>
+                <span>
+                  <svg fill="#000000" width="18" height="18" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M27 4.96h-5.975v-1.918c0-1.655-1.346-3-3-3h-3.989c-1.655 0-3 1.345-3 3v1.918h-6.037c-1.104 0-2 0.896-2 2v22.999c0 1.105 0.896 2 2 2h22c1.105 0 2-0.895 2-2v-22.999c0-1.104-0.895-2-2-2h0zM13.037 3.042c0-0.552 0.448-1 1-1h3.989c0.552 0 1 0.448 1 1v1.918h-5.989v-1.918zM27 29.959h-22v-22.999h6.037v2.058s-0.027 0.999 0.994 0.999c1.125 0 1.006-0.999 1.006-0.999v-2.058h5.989v2.058s-0.067 1.004 0.996 1.004c1 0 1.004-1.004 1.004-1.004v-2.058h5.974v22.999z"></path>
+                  </svg>
+                </span>
+              </div>
               <div>
 
               </div>
             </div>
-            <div>
 
+            {/* Tab when screen comes down to mobile view */}
+            <div className={styles['mobileView-bottom-navigation']}>
+              <div className={styles['bottom-navigation-inner']}>
+                <span>
+                  <Link to={'/'} >
+                    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 7.90637V18C19 19.1046 18.1046 20 17 20H7C5.89543 20 5 19.1046 5 18V7.90637M2 10.0001L10.8531 3.80297C11.5417 3.32092 12.4583 3.32092 13.1469 3.80297L22 10.0001" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <p>HOME</p>
+                  </Link>
+                </span>
+                <span>
+                  <Link to={'/'} >
+                    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 7.90637V18C19 19.1046 18.1046 20 17 20H7C5.89543 20 5 19.1046 5 18V7.90637M2 10.0001L10.8531 3.80297C11.5417 3.32092 12.4583 3.32092 13.1469 3.80297L22 10.0001" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <p>HOME</p>
+                  </Link>
+                </span>
+                <span>
+                  <Link to={'/account'}>
+                    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <p>ACCOUNT</p>
+                  </Link>
+                </span>
+                <span>
+                  <Link to={'/wishlist'}>
+                    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" clip-rule="evenodd" d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <p>WISHLIST</p>
+                  </Link>
+                </span>
+                <span>
+                  <Link to={'/cart'}>
+                    <svg fill="#000000" width="18" height="18" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M27 4.96h-5.975v-1.918c0-1.655-1.346-3-3-3h-3.989c-1.655 0-3 1.345-3 3v1.918h-6.037c-1.104 0-2 0.896-2 2v22.999c0 1.105 0.896 2 2 2h22c1.105 0 2-0.895 2-2v-22.999c0-1.104-0.895-2-2-2h0zM13.037 3.042c0-0.552 0.448-1 1-1h3.989c0.552 0 1 0.448 1 1v1.918h-5.989v-1.918zM27 29.959h-22v-22.999h6.037v2.058s-0.027 0.999 0.994 0.999c1.125 0 1.006-0.999 1.006-0.999v-2.058h5.989v2.058s-0.067 1.004 0.996 1.004c1 0 1.004-1.004 1.004-1.004v-2.058h5.974v22.999z"></path>
+                    </svg>
+                    <p>CART</p>
+                  </Link>
+                </span>
+              </div>
             </div>
           </div>
         </div>
